9dd25b45ad5cbfcc32bb9e95cfd934ed91391501
nhonvo <vothuongtruongnhon2002@gmail.com>
2024-04-14 20:13:43 +0700

    enhance seed data

--- Persistence/Seed.cs
+++ Persistence/Seed.cs
@@ -14,239 +14,248 @@ namespace Persistence
                 {
                     new AppUser
                     {
-                        DisplayName = "Bob",
-                        UserName = "bob",
-                        Email = "bob@test.com"
+                        DisplayName = "truongnhon",
+                        UserName = "truongnhon",
+                        Email = "truongnhon@gmail.com"
                     },
                     new AppUser
                     {
-                        DisplayName = "Jane",
-                        UserName = "jane",
-                        Email = "jane@test.com"
+                        DisplayName = "ducdung",
+                        UserName = "ducdung",
+                        Email = "ducdung@gmail.com"
                     },
                     new AppUser
                     {
-                        DisplayName = "Tom",
-                        UserName = "tom",
-                        Email = "tom@test.com"
+                        DisplayName = "phamtuan",
+                        UserName = "phamtuan",
+                        Email = "phamtuan@gmail.com"
                     },
                 };
 
                 foreach (var user in users)
                 {
-                    await userManager.CreateAsync(user, "Pa$$w0rd");
+                    await userManager.CreateAsync(user, "P@ssw0rd");
                 }
-
                 var activities = new List<Activity>
                 {
                     new Activity
                     {
-                        Title = "Past Activity 1",
+                        Title = "Dã ngoại và Picnic tại Vườn Hoa Cẩm Tú",
                         Date = DateTime.UtcNow.AddMonths(-2),
-                        Description = "Activity 2 months ago",
-                        Category = "drinks",
-                        City = "London",
-                        Venue = "Pub",
+                        Description = "Thư giãn và thưởng thức picnic giữa thiên nhiên",
+                        Category = "outdoors",
+                        City = "Hà Nội",
+                        Venue = "Vườn Hoa Cẩm Tú",
                         Attendees = new List<ActivityAttendee>
                         {
                             new ActivityAttendee
                             {
-                                AppUser = users[0],
+                                AppUser = users[0], 
                                 IsHost = true
+                            },
+                            new ActivityAttendee
+                            {
+                                AppUser = users[1], 
+                                IsHost = false
                             }
                         }
                     },
                     new Activity
                     {
-                        Title = "Past Activity 2",
+                        Title = "Dạo chơi và Tham quan Bảo tàng Dân tộc học Việt Nam",
                         Date = DateTime.UtcNow.AddMonths(-1),
-                        Description = "Activity 1 month ago",
+                        Description = "Khám phá văn hóa dân tộc Việt Nam tại Bảo tàng Dân tộc học",
                         Category = "culture",
-                        City = "Paris",
-                        Venue = "The Louvre",
+                        City = "Hà Nội",
+                        Venue = "Bảo tàng Dân tộc học Việt Nam",
                         Attendees = new List<ActivityAttendee>
                         {
                             new ActivityAttendee
                             {
-                                AppUser = users[0],
+                                AppUser = users[0], 
                                 IsHost = true
                             },
                             new ActivityAttendee
                             {
-                                AppUser = users[1],
+                                AppUser = users[1], 
                                 IsHost = false
-                            },
-                        }
-                    },
-                    new Activity
-                    {
-                        Title = "Future Activity 1",
-                        Date = DateTime.UtcNow.AddMonths(1),
-                        Description = "Activity 1 month in future",
-                        Category = "music",
-                        City = "London",
-                        Venue = "Wembly Stadium",
-                        Attendees = new List<ActivityAttendee>
-                        {
-                            new ActivityAttendee
-                            {
-                                AppUser = users[2],
-                                IsHost = true
-                            },
-                            new ActivityAttendee
-                            {
-                                AppUser = users[1],
-                                IsHost = false
-                            },
-                        }
-                    },
-                    new Activity
-                    {
-                        Title = "Future Activity 2",
-                        Date = DateTime.UtcNow.AddMonths(2),
-                        Description = "Activity 2 months in future",
-                        Category = "food",
-                        City = "London",
-                        Venue = "Jamies Italian",
-                        Attendees = new List<ActivityAttendee>
-                        {
-                            new ActivityAttendee
-                            {
-                                AppUser = users[0],
-                                IsHost = true
-                            },
-                            new ActivityAttendee
-                            {
-                                AppUser = users[2],
-                                IsHost = false
-                            },
-                        }
-                    },
-                    new Activity
-                    {
-                        Title = "Future Activity 3",
-                        Date = DateTime.UtcNow.AddMonths(3),
-                        Description = "Activity 3 months in future",
-                        Category = "drinks",
-                        City = "London",
-                        Venue = "Pub",
-                        Attendees = new List<ActivityAttendee>
-                        {
-                            new ActivityAttendee
-                            {
-                                AppUser = users[1],
-                                IsHost = true
-                            },
-                            new ActivityAttendee
-                            {
-                                AppUser = users[0],
-                                IsHost = false
-                            },
-                        }
-                    },
-                    new Activity
-                    {
-                        Title = "Future Activity 4",
-                        Date = DateTime.UtcNow.AddMonths(4),
-                        Description = "Activity 4 months in future",
-                        Category = "culture",
-                        City = "London",
-                        Venue = "British Museum",
-                        Attendees = new List<ActivityAttendee>
-                        {
-                            new ActivityAttendee
-                            {
-                                AppUser = users[1],
-                                IsHost = true
                             }
                         }
                     },
                     new Activity
                     {
-                        Title = "Future Activity 5",
-                        Date = DateTime.UtcNow.AddMonths(5),
-                        Description = "Activity 5 months in future",
-                        Category = "drinks",
-                        City = "London",
-                        Venue = "Punch and Judy",
-                        Attendees = new List<ActivityAttendee>
-                        {
-                            new ActivityAttendee
-                            {
-                                AppUser = users[0],
-                                IsHost = true
-                            },
-                            new ActivityAttendee
-                            {
-                                AppUser = users[1],
-                                IsHost = false
-                            },
-                        }
-                    },
-                    new Activity
-                    {
-                        Title = "Future Activity 6",
-                        Date = DateTime.UtcNow.AddMonths(6),
-                        Description = "Activity 6 months in future",
+                        Title = "Đêm nhạc Rock tại Câu lạc bộ Rock Fanatics",
+                        Date = DateTime.UtcNow.AddMonths(1),
+                        Description = "Trải nghiệm không khí sôi động với âm nhạc Rock tại Câu lạc bộ Rock Fanatics",
                         Category = "music",
-                        City = "London",
-                        Venue = "O2 Arena",
+                        City = "Hồ Chí Minh",
+                        Venue = "Câu lạc bộ Rock Fanatics",
                         Attendees = new List<ActivityAttendee>
                         {
                             new ActivityAttendee
                             {
-                                AppUser = users[2],
+                                AppUser = users[2], 
                                 IsHost = true
                             },
                             new ActivityAttendee
                             {
-                                AppUser = users[1],
+                                AppUser = users[1], 
                                 IsHost = false
-                            },
+                            }
                         }
                     },
                     new Activity
                     {
-                        Title = "Future Activity 7",
+                        Title = "Lớp Học Nấu Ăn: Ẩm thực Miền Trung",
+                        Date = DateTime.UtcNow.AddMonths(2),
+                        Description = "Học cách chế biến các món ăn truyền thống từ Miền Trung",
+                        Category = "food",
+                        City = "Đà Nẵng",
+                        Venue = "Trường Dạy Nấu Ăn Miền Trung",
+                        Attendees = new List<ActivityAttendee>
+                        {
+                            new ActivityAttendee
+                            {
+                                AppUser = users[0], 
+                                IsHost = true
+                            },
+                            new ActivityAttendee
+                            {
+                                AppUser = users[2], 
+                                IsHost = false
+                            }
+                        }
+                    },
+                    new Activity
+                    {
+                        Title = "Tham quan Làng Nghề Lịch Sử Dương Lâm",
+                        Date = DateTime.UtcNow.AddMonths(3),
+                        Description = "Khám phá nghề truyền thống và văn hóa tại làng nghề Dương Lâm",
+                        Category = "culture",
+                        City = "Hà Nội",
+                        Venue = "Làng Nghề Dương Lâm",
+                        Attendees = new List<ActivityAttendee>
+                        {
+                            new ActivityAttendee
+                            {
+                                AppUser = users[1], 
+                                IsHost = true
+                            },
+                            new ActivityAttendee
+                            {
+                                AppUser = users[0], 
+                                IsHost = false
+                            }
+                        }
+                    },
+                    new Activity
+                    {
+                        Title = "Dã ngoại và Lều trại tại Vịnh Hạ Long",
+                        Date = DateTime.UtcNow.AddMonths(4),
+                        Description = "Trải nghiệm cuộc sống ngoài trời và ngủ dưới bầu trời đêm tại Vịnh Hạ Long",
+                        Category = "outdoors",
+                        City = "Quảng Ninh",
+                        Venue = "Vịnh Hạ Long",
+                        Attendees = new List<ActivityAttendee>
+                        {
+                            new ActivityAttendee
+                            {
+                                AppUser = users[2], 
+                                IsHost = true
+                            },
+                            new ActivityAttendee
+                            {
+                                AppUser = users[1], 
+                                IsHost = false
+                            }
+                        }
+                    },
+                    new Activity
+                    {
+                        Title = "Trải nghiệm Văn hóa Dân dụng tại Làng Gốm Bát Tràng",
+                        Date = DateTime.UtcNow.AddMonths(5),
+                        Description = "Học cách làm gốm và hiểu sâu hơn về nghề gốm truyền thống",
+                        Category = "culture",
+                        City = "Hà Nội",
+                        Venue = "Làng Gốm Bát Tràng",
+                        Attendees = new List<ActivityAttendee>
+                        {
+                            new ActivityAttendee
+                            {
+                                AppUser = users[0], 
+                                IsHost = true
+                            },
+                            new ActivityAttendee
+                            {
+                                AppUser = users[2], 
+                                IsHost = false
+                            }
+                        }
+                    },
+                    new Activity
+                    {
+                        Title = "Du lịch Mạo Hiểm: Leo núi Fansipan",
+                        Date = DateTime.UtcNow.AddMonths(6),
+                        Description = "Chinh phục đỉnh cao Việt Nam với chuyến leo núi Fansipan",
+                        Category = "outdoors",
+                        City = "Lào Cai",
+                        Venue = "Núi Fansipan",
+                        Attendees = new List<ActivityAttendee>
+                        {
+                            new ActivityAttendee
+                            {
+                                AppUser = users[1], 
+                                IsHost = true
+                            },
+                            new ActivityAttendee
+                            {
+                                AppUser = users[0], 
+                                IsHost = false
+                            }
+                        }
+                    },
+                    new Activity
+                    {
+                        Title = "Lễ Hội Múa Lân Truyền Thống",
                         Date = DateTime.UtcNow.AddMonths(7),
-                        Description = "Activity 7 months in future",
-                        Category = "travel",
-                        City = "Berlin",
-                        Venue = "All",
+                        Description = "Tham gia lễ hội múa lân truyền thống và khám phá nghệ thuật văn hóa đặc sắc",
+                        Category = "culture",
+                        City = "Hồ Chí Minh",
+                        Venue = "Sân khấu trung tâm TP.HCM",
                         Attendees = new List<ActivityAttendee>
                         {
                             new ActivityAttendee
                             {
-                                AppUser = users[0],
+                                AppUser = users[2], 
                                 IsHost = true
                             },
                             new ActivityAttendee
                             {
-                                AppUser = users[2],
+                                AppUser = users[1], 
                                 IsHost = false
-                            },
+                            }
                         }
                     },
                     new Activity
                     {
-                        Title = "Future Activity 8",
+                        Title = "Đêm Nhạc EDM tại Bar Club Volcano",
                         Date = DateTime.UtcNow.AddMonths(8),
-                        Description = "Activity 8 months in future",
-                        Category = "drinks",
-                        City = "London",
-                        Venue = "Pub",
+                        Description = "Trải nghiệm không khí sôi động với nhạc EDM và đồ uống tại Bar Club Volcano",
+                        Category = "music",
+                        City = "Đà Nẵng",
+                        Venue = "Bar Club Volcano",
                         Attendees = new List<ActivityAttendee>
                         {
                             new ActivityAttendee
                             {
-                                AppUser = users[2],
+                                AppUser = users[0], 
                                 IsHost = true
                             },
                             new ActivityAttendee
                             {
-                                AppUser = users[1],
+                                AppUser = users[2], 
                                 IsHost = false
-                            },
+                            }
                         }
                     }
                 };

e9c3987ae466e89bbf1fe9b4bd534920863ebffc
nhonvo <vothuongtruongnhon2002@gmail.com>
2024-04-14 20:59:17 +0700

    update cnd link

--- Infrastructure/Photos/CloudinarySettings.cs
+++ Infrastructure/Photos/CloudinarySettings.cs
@@ -2,8 +2,8 @@
 {
     public class CloudinarySettings
     {
-        public string CloudName { get; set; }
-        public string ApiKey { get; set; }
-        public string ApiSecret { get; set; }
+        public string CloudName { get; set; } = "dwsrblpdl";
+        public string ApiKey { get; set; } = "656264362814224";
+        public string ApiSecret { get; set; } = "EnuCYquqozVLohNebgOdBhB_M0w";
     }
 }
